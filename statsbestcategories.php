1934533252afad67a6e782aa46cf50ed04a38fc8
Michael Dekker <git@michaeldekker.nl>
2017-02-05 07:41:27 +0100

    Update statsbestcategories.php

--- statsbestcategories.php
+++ statsbestcategories.php
@@ -24,7 +24,7 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
-if (!defined('_PS_VERSION_'))
+if (!defined('_TB_VERSION_'))
 	exit;
 
 class StatsBestCategories extends ModuleGrid
@@ -41,8 +41,8 @@ class StatsBestCategories extends ModuleGrid
 	{
 		$this->name = 'statsbestcategories';
 		$this->tab = 'analytics_stats';
-		$this->version = '1.5.0';
-		$this->author = 'PrestaShop';
+		$this->version = '2.0.0';
+		$this->author = 'thirty bees';
 		$this->need_instance = 0;
 
 		parent::__construct();
